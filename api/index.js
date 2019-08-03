314ba677323c8b5af474c66b73e1dc8daa37fd19
Allen Hai <allenhai03@gmail.com>
2019-08-02 20:02:52 -0500

    add playground and instrospection

--- api/index.js
+++ api/index.js
@@ -16,6 +16,11 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
 
 module.exports = micro(cors(server.createHandler()));
